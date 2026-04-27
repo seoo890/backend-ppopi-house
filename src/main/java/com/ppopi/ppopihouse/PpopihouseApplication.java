4a1fc57165ac836dfd3178ba5ce585a82beb8659
10Hyuna <kimhaa1005@naver.com>
2026-04-27 23:13:44 +0900

    init: 스프링 부트 프로젝트 초기화

---
+++ src/main/java/com/ppopi/ppopihouse/PpopihouseApplication.java
@@ -0,0 +1,13 @@
+package com.ppopi.ppopihouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpopihouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpopihouseApplication.class, args);
+    }
+
+}
