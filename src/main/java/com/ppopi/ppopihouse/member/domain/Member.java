6d22134f0d9a0573e57b101fbaab65a8357e9914
10Hyuna <kimhaa1005@naver.com>
2026-04-28 00:17:43 +0900

    feat(member): 회원 엔티티 추가

---
+++ src/main/java/com/ppopi/ppopihouse/member/domain/Member.java
@@ -0,0 +1,22 @@
+package com.ppopi.ppopihouse.member.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "member")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long memberId;
+
+    @Column(name = "kakao_user_id", nullable = false, unique = true)
+    private String kakaoUserId;
+}
