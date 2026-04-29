39fab655d0ea0743bd381e2e8dca876f6aa9d6b6
10Hyuna <kimhaa1005@naver.com>
2026-04-29 15:30:22 +0900

    feat(diagnosis): Diagnosis 레포지토리 추가

--- src/main/java/com/ppopi/ppopihouse/diagnosis/repository/DiagnosisRepository.java
+++ src/main/java/com/ppopi/ppopihouse/diagnosis/repository/DiagnosisRepository.java
@@ -1,2 +1,7 @@
-package com.ppopi.ppopihouse.diagnosis.repository;public interface DiagnosisRepository {
+package com.ppopi.ppopihouse.diagnosis.repository;
+
+import com.ppopi.ppopihouse.diagnosis.domain.Diagnosis;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DiagnosisRepository extends JpaRepository<Diagnosis, Long> {
 }
