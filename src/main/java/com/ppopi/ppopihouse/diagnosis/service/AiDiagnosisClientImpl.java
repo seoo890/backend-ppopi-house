a17beca7f455903520e24a3169cbed23582ea62b
10Hyuna <kimhaa1005@naver.com>
2026-05-02 00:31:36 +0900

    feat(diagnosis): ai 서버 연동 서비스 로직

---
+++ src/main/java/com/ppopi/ppopihouse/diagnosis/service/AiDiagnosisClientImpl.java
@@ -0,0 +1,28 @@
+package com.ppopi.ppopihouse.diagnosis.service;
+
+import com.ppopi.ppopihouse.diagnosis.dto.external.AiDiagnosisRequest;
+import com.ppopi.ppopihouse.diagnosis.dto.external.AiDiagnosisResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+@RequiredArgsConstructor
+public class AiDiagnosisClientImpl implements AiDiagnosisClient {
+
+    private final WebClient.Builder webClientBuilder;
+
+    @Value("${external.ai-api.base-url}")
+    private String aiApiBaseUrl;
+
+    public AiDiagnosisResponse diagnose(AiDiagnosisRequest request) {
+        return webClientBuilder.build()
+                .post()
+                .uri(aiApiBaseUrl + "/diagnose")
+                .bodyValue(request)
+                .retrieve()
+                .bodyToMono(AiDiagnosisResponse.class)
+                .block();
+    }
+}
