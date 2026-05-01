ae90fefe12af13ada4717885c878b617999515e8
10Hyuna <kimhaa1005@naver.com>
2026-05-02 00:30:21 +0900

    feat(diagnosis): api 요청에 따라 필요한 dto 구조 정의

---
+++ src/main/java/com/ppopi/ppopihouse/diagnosis/dto/response/DiagnosisResponse.java
@@ -0,0 +1,17 @@
+package com.ppopi.ppopihouse.diagnosis.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class DiagnosisResponse {
+
+    private String diseaseName;
+    private String triage;
+    private float confidence;
+    private String affectedArea;
+    private String description;
+    private String action;
+
+}
