2d7d99bad149d5460c827a51326990ac709f080e
10Hyuna <kimhaa1005@naver.com>
2026-05-03 00:25:09 +0900

    feat(security): swagger에 Authorize 버튼 추가

--- src/main/java/com/ppopi/ppopihouse/global/config/SwaggerConfig.java
+++ src/main/java/com/ppopi/ppopihouse/global/config/SwaggerConfig.java
@@ -1,16 +1,32 @@
 package com.ppopi.ppopihouse.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    @Bean
     public OpenAPI ppopiHouseOpenAPI() {
+        String schemeName = "JWT";
+
         return new OpenAPI()
                 .info(new Info()
                         .title("뽀삐의 집 API")
                         .description("뽀삐의 집 캡스톤 프로젝트 API 문서")
-                        .version("v1.0.0"));
+                        .version("v1.0.0"))
+                .components(new Components()
+                        .addSecuritySchemes(schemeName,
+                                new SecurityScheme()
+                                        .name("Authorization")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList(schemeName));
     }
 }
